15a14ae9b5d5fdd2286b4db22e09765b2e4b30df
lantrinhca <lantrinh.ca@gmail.com>
2015-07-28 14:50:32 +0000

    Building a Model Test

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,4 +4,10 @@ class Quote < ActiveRecord::Base
 	validates :saying, length: { in: 3..140}
 	validates :author, length: { in: 3..50}
 
+	def unique_tag
+	  abbr = self.author.split(" ").collect do |sub_string|
+			sub_string[0]
+		end
+		abbr.join + '#' + self.id.to_s
+	end
 end
