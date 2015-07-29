1c05aa44f59fd2e0214b5fc7493c7b8f6ee07cf1
lantrinhca <lantrinh.ca@gmail.com>
2015-07-29 14:29:08 +0000

    installed Factory Girl

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,7 +1,6 @@
 class QuotesController < ApplicationController
 	def index
 		@quote = Quote.order("RANDOM()").first
-
 	end
 
 	def create
@@ -12,9 +11,15 @@ class QuotesController < ApplicationController
 		redirect_to root_path
 	end
 
-	# def about
-	#
-	# end
+	def about
+	end
+
+	def show
+		@quote = Quote.where(:id => params[:id]).first
+		if @quote.blank?
+			render :text => "Not Found", :status => :not_found	
+		end	
+	end
 
 	private
 
